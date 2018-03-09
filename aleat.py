5bf8240679910a0cf6d35b6e72e95ba9321dc7d7
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha29.aulas.gsyc.urjc.es>
2018-03-09 18:56:39 +0100

    Try indented and object created properly

--- aleat.py
+++ aleat.py
@@ -28,24 +28,24 @@ def process(self, parsedRequest):
 
 if __name__ == "__main__":
 
-try:
-    while True:
-        myApp = webapp()
-        # print('Waiting for connections')
-        # (recvSocket, address) = mySocket.accept()
-        # print('Request received:')
-        # print(recvSocket.recv(2048))
-        # print('Answering back...')
-        #
-        # rnd_num = random.randint(1, 9999999)
-        #
-        # recvSocket.send(bytes(
-        #                 "HTTP/1.1 200 OK\r\n\r\n" +
-        #                 "<html><body><h1>Hola!</h1>" +
-        #                 "<a href=" + str(rnd_num) + ">Dame otra</a>" +
-        #                 "</body></html>" +
-        #                 "\r\n", "utf-8"))
-        # recvSocket.close()
-except KeyboardInterrupt:
-    print("Closing binded socket")
-    mySocket.close()
+    try:
+        while True:
+            myApp = webapp("localhost", 1234))
+            # print('Waiting for connections')
+            # (recvSocket, address) = mySocket.accept()
+            # print('Request received:')
+            # print(recvSocket.recv(2048))
+            # print('Answering back...')
+            #
+            # rnd_num = random.randint(1, 9999999)
+            #
+            # recvSocket.send(bytes(
+            #                 "HTTP/1.1 200 OK\r\n\r\n" +
+            #                 "<html><body><h1>Hola!</h1>" +
+            #                 "<a href=" + str(rnd_num) + ">Dame otra</a>" +
+            #                 "</body></html>" +
+            #                 "\r\n", "utf-8"))
+            # recvSocket.close()
+    except KeyboardInterrupt:
+        print("Closing binded socket")
+        mySocket.close()
