d87706808ecea5249d592aee9ae94f8af236ab27
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha29.aulas.gsyc.urjc.es>
2018-03-09 19:06:22 +0100

    Class was not being called properly

--- aleat.py
+++ aleat.py
@@ -7,24 +7,23 @@ r.pachecom @ gsyc.es
 SAT subject (Universidad Rey Juan Carlos)
 """
 
-import socket
 import random
 import webapp
 
-class myApp(webapp.webApp)
-    def parse(self, request):
-        try:
-            method = received.split()[0]
-            resource = received.split()[1]
-            return(method, resource)
-        except:
-            return('', '')
+class randomURL(webapp.webApp):
+    # def parse(self, request):
+    #     try:
+    #         method = received.split()[0]
+    #         resource = received.split()[1]
+    #         return(method, resource)
+    #     except:
+    #         return('', '')
 
-    def process(parsedRequest):
+    def process(self, parsedRequest):
         rnd_num = random.randint(1, 9999999)
-        return ("200 OK", "<html><body><h1>Hola!</h1>" +
-                          "<a href=" + str(rnd_num) + ">Dame otra</a>" +
+        return ("200 OK", "<html><body><h1>" +
+                          "<a href=" + str(rnd_num) + " > Dame otra</a></h1>" +
                           "</body></html>")
 
 if __name__ == "__main__":
-    myApp = webapp.webApp("localhost", 1234)
+    myApp = randomURL("localhost", 1234)
