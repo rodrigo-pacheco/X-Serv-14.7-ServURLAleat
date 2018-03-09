050c4c926b3361817c6dddb5afb58b0a3f4148cb
Rodrigo Pacheco Martinez Atienza <rpacheco@alpha29.aulas.gsyc.urjc.es>
2018-03-09 18:53:42 +0100

    Process and parse implemented. Object created

--- aleat.py
+++ aleat.py
@@ -9,40 +9,43 @@ SAT subject (Universidad Rey Juan Carlos)
 
 import socket
 import random
+import webapp
 
-# Create a TCP objet socket and bind it to a port
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
 
-mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Let the port be reused if no process is actually using it
-mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Bind to the address corresponding to the main name of the host
-mySocket.bind(('localhost', 1234))
+def parse:
+    try:
+        method = received.split()[0]
+        resource = received.split()[1]
+        return(method, resource)
+    except:
+        return('', '')
 
-# Queue a maximum of 5 TCP connection requests
-mySocket.listen(5)
+def process:
+    rnd_num = random.randint(1, 9999999)
+    return ("200 OK", "<html><body><h1>Hola!</h1>" +
+                      "<a href=" + str(rnd_num) + ">Dame otra</a>" +
+                      "</body></html>")
 
-# Accept connections, read incoming data, and answer back an HTML page
-#  (in an almost-infinite loop; the loop can be stopped with Ctrl+C)
+if __name__ == "__main__":
 
 try:
     while True:
-        print('Waiting for connections')
-        (recvSocket, address) = mySocket.accept()
-        print('Request received:')
-        print(recvSocket.recv(2048))
-        print('Answering back...')
-
-        rnd_num = random.randint(1, 9999999)
-
-        recvSocket.send(bytes(
-                        "HTTP/1.1 200 OK\r\n\r\n" +
-                        "<html><body><h1>Hola!</h1>" +
-                        "<a href=" + str(rnd_num) + ">Dame otra</a>" +
-                        "</body></html>" +
-                        "\r\n", "utf-8"))
-        recvSocket.close()
+        myApp = webapp()
+        # print('Waiting for connections')
+        # (recvSocket, address) = mySocket.accept()
+        # print('Request received:')
+        # print(recvSocket.recv(2048))
+        # print('Answering back...')
+        #
+        # rnd_num = random.randint(1, 9999999)
+        #
+        # recvSocket.send(bytes(
+        #                 "HTTP/1.1 200 OK\r\n\r\n" +
+        #                 "<html><body><h1>Hola!</h1>" +
+        #                 "<a href=" + str(rnd_num) + ">Dame otra</a>" +
+        #                 "</body></html>" +
+        #                 "\r\n", "utf-8"))
+        # recvSocket.close()
 except KeyboardInterrupt:
     print("Closing binded socket")
     mySocket.close()
